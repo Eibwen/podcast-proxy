f2194700ad613702d0485b1b4eee620518c265ac
Greg Walker <Eibwen@users.noreply.github.com>
2024-05-24 16:16:20 -0500

    initial commit

---
+++ DownloadAndCropFeed.py
@@ -0,0 +1,69 @@
+import requests
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+import os
+import zstandard as zstd
+
+ITEMS_TO_KEEP = 14
+
+feedsToProxy = [
+    "https://feeds.simplecast.com/bOogzwqU"
+]
+
+
+def ProcessUrl(url):
+	# TODO make sure this has any gzip headers enabled?
+	f = DownloadFile(url)
+
+	print(get_file_sizes(f))
+
+	tree = ET.parse(f)
+	root = tree.getroot()
+
+	channelRoot = root.find('channel')
+	items = channelRoot.findall('item')
+
+	itemsToRemove = items[ITEMS_TO_KEEP:]
+
+	for item in itemsToRemove:
+		# print(item.find('title').text)
+		channelRoot.remove(item)
+
+	outputItems = channelRoot.findall('item')
+	print('Output items len: ', len(outputItems))
+
+	outFile = Path(f).with_suffix('.proxy.xml')
+	tree.write(outFile)
+
+	print(get_file_sizes(outFile))
+
+	print('DONE')
+
+
+def DownloadFile(url):
+    local_filename = url.split('/')[-1] + '.xml'
+    sub_folder = 
+    r = requests.get(url)
+    # print(r.headers)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+    return local_filename
+
+
+def get_file_sizes(file_path):
+    # Get the uncompressed size using os.path.getsize()
+    uncompressed_size = os.path.getsize(file_path)
+ 
+    # Get the compressed size using zstandard
+    with open(file_path, 'rb') as file:
+        compressed_data = zstd.compress(file.read())
+        compressed_size = len(compressed_data)
+ 
+    return uncompressed_size, compressed_size, compressed_size*100/uncompressed_size
+
+
+for url in feedsToProxy:
+    ProcessUrl(url)
